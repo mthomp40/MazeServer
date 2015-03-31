dad80d7d1f4233f37988b10b96452e03bc880f75
user <user@UOWUbuntuLinux>
2015-03-31 19:58:18 +1100

    Adding stuff

--- lib/WebSocket/Application/MazeApplication.php
+++ lib/WebSocket/Application/MazeApplication.php
@@ -8,6 +8,20 @@ class MazeApplication extends Application {
     private static $colourNamesArray = array('red', 'green', 'blue', 'aqua',
         'fuschia', 'lime', 'maroon', 'navy', 'yellow');
     private static $colourcounter = 0;
+    private static $mazemap = [// 1  2  3  4  5  6  7  8  9
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,], // 0
+        [1, 0, 1, 0, 0, 1, 0, 0, 0, 1,], // 1
+        [1, 1, 1, 0, 0, 1, 0, 0, 0, 1,], // 2
+        [1, 0, 1, 0, 0, 1, 1, 0, 0, 1,], // 3
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 1,], // 4
+        [1, 0, 1, 0, 0, 0, 1, 1, 1, 1,], // 5
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 1,], // 6
+        [1, 0, 1, 0, 0, 1, 0, 1, 0, 1,], // 7
+        [1, 0, 1, 0, 0, 1, 0, 1, 0, 1,], // 8
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,], // 9
+    ];
+    //private $mapW = $mazemap.length;
+    //private $mapH = $mazemap[0].length;
     private $_clients = array();
 
     public function onConnect($client) {
@@ -20,8 +34,9 @@ class MazeApplication extends Application {
         $info['colour'] = MazeApplication::$colourNamesArray[MazeApplication::$colourcounter];
 
         MazeApplication::$colourcounter++;
-        if (MazeApplication::$colourcounter == 9)
+        if (MazeApplication::$colourcounter == 9) {
             MazeApplication::$colourcounter = 0;
+        }
         $client->setClientInfo($info);
     }
 
@@ -53,20 +68,29 @@ class MazeApplication extends Application {
 //    }
 
     private function _process($client, $action, $info) {
-        // Currently the only action defined is "Move"
-        // First, update the client record
+        if ($action == "login") {
+            foreach ($this->_clients as $aclient) {
+                $infoarray = $aclient->getClientInfo();
+                if (isset($infoarray['uname']) && $infoarray['uname'] === $info['uname']) {
+                    $client->send($this->_encodeData('alreadyinuse', null));
+                    return;
+                }
+            }
+            $id = $client->getClientId();
+            $this->_clients[$id] = $client;
+            $infoarray = $client->getClientInfo();
+            $infoarray['uname'] = $info['uname'];
+            $infoarray['mazemap'] = json_encode(MazeApplication::$mazemap);
+            $client->setClientInfo($infoarray);
+            $updateData = $this->_composeUpdateMessage();
+            $encodedUpdate = $this->_encodeData('setupmap', $updateData);
+            $client->send($encodedUpdate);
+        }
         $infoarray = $client->getClientInfo();
         $infoarray['uname'] = $info['uname'];
         $client->setClientInfo($infoarray);
-
-        // Next, compose a data message block that contains details of all
-        // clients
         $updateData = $this->_composeUpdateMessage();
-        // Data block gets placed in a message struction with action
-        // and data fields; only action for this application is 'update'
-        $encodedUpdate = $this->_encodeData('login', $updateData);
-
-        // Now send that to all connected clients
+        $encodedUpdate = $this->_encodeData('update', $updateData);
         foreach ($this->_clients as $sendto) {
             $sendto->send($encodedUpdate);
         }
